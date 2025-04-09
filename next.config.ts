d7e30739dd13ff6628041615b8a585fb4bece79a
Arjun Avtani <arjun@Arjuns-Mac.local>
2025-04-09 14:26:29 +1000

    next commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    // Customize rules as needed
+  },
 };
-
-export default nextConfig;
